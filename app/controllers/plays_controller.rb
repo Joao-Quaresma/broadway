0bc86aa861372cd46e93af81153e0a4ba937ba80
Joao Quaresma <joaoquaresma@Joaos-MacBook-Pro-3.local>
2018-08-17 08:40:11 +0100

    add nav and associations

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -9,11 +9,11 @@ class PlaysController < ApplicationController
   end
 
   def new
-    @play = Play.new
+    @play = Play.current_user.play.build
   end
 
   def create
-    @play = Play.new(play_params)
+    @play = Play.current_user.play.build(play_params)
     if @play.save
       redirect_to root_path
     else
