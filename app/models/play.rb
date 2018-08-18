2e1d82c3742d97e40bef6b88861fcd83aa9d898f
Joao Quaresma <joaoquaresma@Joaos-MacBook-Pro-3.local>
2018-08-18 20:36:00 +0100

    add paperclip to app and change view files

--- app/models/play.rb
+++ app/models/play.rb
@@ -4,4 +4,6 @@
 class Play < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_attached_file :play_img, styles: { play_index: '250x300>', play_show: '325x475>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :play_img, content_type: %r{/\Aimage\/.*\z/}
 end
