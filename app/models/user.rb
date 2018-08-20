ae3d364f2d2e15df26d14947a60e4138e1459630
Joao Quaresma <joaoquaresma@Joaos-MacBook-Pro-3.local>
2018-08-20 08:26:26 +0100

    Review model, migrations and model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # User model
 class User < ApplicationRecord
   has_many :plays
+  has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
